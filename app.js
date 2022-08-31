e23c2a3c1ff96ec65ecacd9a7ff7a0636405215e
faze20 <42577269+faze20@users.noreply.github.com>
2022-08-30 18:04:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function age(){
+    var d1 = document.getElementById('date').value;
+    var m1 = document.getElementById('month').value;
+    var y1 = document.getElementById('year').value;
+
+    var date= new Date();
+    var d2 = date.getDate();
+
+    var m2 = 1 + date.getMonth();
+    var y2 = date.getFullYear();
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if(d1 > d2){
+        d2 = d2 + month[ m2 - 1]
+        m2 = m2 - 1
+    }
+    if(m1 > m2){
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+    }
+    var day = d2 - d1;
+    var m = m2 - m1;
+    var year = y2 - y1 ;
+
+    document.getElementById('age').innerHTML = 'Your Age is ' + year + ' Years ' + m + ' Months ' + day + ' Days'
+}
+function reset(){
+    document.getElementById('myform').reset();
+}
